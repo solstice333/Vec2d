9f79ecbce0e00557375438f0bb10002188b4235f
Kevin Navero <knavero@gmail.com>
2015-08-23 08:01:10 -0700

    added way to get end iter

--- foo.cpp
+++ foo.cpp
@@ -7,46 +7,12 @@
 
 using namespace std;
 
-// iterator_tests_int_array() : iterator tests using single dimension vector
-void iterator_tests_int_array() {
-   int my_ints[] = {0, 1, 2, 3, 4};
-   Iter<int> my_iter_beg(my_ints); 
-   Iter<int> my_iter_end(my_ints + sizeof(my_ints)/sizeof(int));
-
-   Iter<int> my_iter_beg1(my_iter_beg);
-   Iter<int> my_iter_end1(my_ints + sizeof(my_ints)/sizeof(int));
-
-   assert(my_iter_beg == my_iter_beg1);
-   assert(!(my_iter_beg == my_iter_end));
-   assert(my_iter_beg != my_iter_end);
-   assert(!(my_iter_beg != my_iter_beg1));
-
-   Iter<int> my_iter_next(my_ints + 1);
-   assert(typeid(Iter<int>) == typeid(my_iter_beg));
-   assert(typeid(Iter<int>) == typeid(++my_iter_beg));
-   assert(typeid(Iter<int>) == typeid(my_iter_next));
-
-   assert(my_iter_beg == my_iter_beg1);
-   assert(++my_iter_beg != my_iter_beg1);
-   my_iter_beg = my_ints;
-   assert(my_iter_beg == my_iter_beg1);
-   assert(my_iter_beg++ == my_iter_beg1);
-   assert(my_iter_beg != my_iter_beg1);
-
-   my_iter_beg = my_ints;
-   Iter<int> *my_iter_ptr = new Iter<int>(my_ints);
-   assert(*my_iter_ptr == my_iter_beg);
-   my_iter_ptr->set_iter(my_ints + 1);
-   assert(**my_iter_ptr == 1);
-   delete my_iter_ptr;
-}
-
-void iterator_tests_vector() {
-   vector< vector<int> > v(2);
+void iterator_tests() {
+   vector< vector<int> > vv(2);
    int val = 0;
 
    // populate 2d vec
-   for (auto i = v.begin(), end_i = v.end(); i != end_i; ++i) {
+   for (auto i = vv.begin(), end_i = vv.end(); i != end_i; ++i) {
       i->resize(2);
       for (auto j = i->begin(), end_j = i->end(); j != end_j; ++j)
          *j = ++val;
@@ -54,13 +20,40 @@ void iterator_tests_vector() {
 
    // test print
    val = 0;
-   for (int i = 0; i < v.size(); ++i) {
-      for (int j = 0; j < v[i].size(); ++j)
-         assert(v[i][j] == ++val);
+   cout << "2d vector contents" << endl;
+   for (int i = 0; i < vv.size(); ++i) {
+      for (int j = 0; j < vv[i].size(); ++j) {
+         if (j > 0)
+            cout << ", ";
+         assert(vv[i][j] == ++val);
+         cout << vv[i][j];
+      }
+      cout << endl;
    }
+   cout << "end of contents" << endl;
 
-   // test iterator with 2d vecs
-   // Iterator my_iter(&v);
+   // test iterator prefix increment with 2d vecs
+   Iter<int> itr(vv);
+   int columns;
+
+   if (vv.size() > 0)
+      columns = vv[0].size();
+
+   for (int i = 0; i < vv.size() * columns; ++i, ++itr)
+      assert(*itr == i + 1);
+
+
+   // test iter set_iter, get_end, != overload, post-incr, * deref
+   vector<int> v(2);
+   v[0] = 5;
+   v[1] = 6;
+   vv.push_back(v); 
+
+   itr.set_iter(vv);
+   Iter<int> end(vv, Iter<int>::END);
+
+   for (int val = 1; itr != end; itr++, ++val)
+      assert(*itr == val);
 }
 
 void constructor_tests_vec2d() {
@@ -78,27 +71,5 @@ void constructor_tests_vec2d() {
 
 int main() {
    constructor_tests_vec2d();
-   iterator_tests_int_array();
-   iterator_tests_vector();
-
-   /*
-   vector< vector<int> > v(2);
-   v[0].push_back(1);
-   v[0].push_back(2);
-   v[1].push_back(3);
-   v[1].push_back(4);
-
-   vector<int> v1;
-   v.push_back(v1);
-   v[2].push_back(5);
-   v[2].push_back(6);
-
-   vector< vector<int> >::iterator row, r_end;
-   vector<int>::iterator col, c_end;
-   for (row = v.begin(), r_end = v.end(); row != r_end; ++row) {
-      for (col = row->begin(), c_end = row->end(); col != c_end; ++col) {
-         cout << *col << endl;
-      }
-   }
-   */
+   iterator_tests();
 }
--- iter.h
+++ iter.h
@@ -2,40 +2,66 @@
 #define ITERATOR_H
 
 #include <iterator>
+#include <vector>
 
 using namespace std;
 
 template<typename T>
 class Iter : public iterator<forward_iterator_tag, T> {
 public:
-   Iter(T *i);
+   enum Pos {BEG, END};
+
+   Iter(vector< vector<T> > &vv, Pos p = BEG);
    Iter(const Iter &i);
-   Iter& operator=(T *i);
-   Iter& set_iter(T *i);
+   Iter& operator=(const Iter &i);
+   Iter& set_iter(vector< vector<T> > &vv);
    bool operator!=(const Iter &r);
    bool operator==(const Iter &r);
    Iter& operator++();
    Iter operator++(int zero);
    T& operator*();
+
 private:
-   T *_i;
+   typename vector<T>::iterator _i;
+   vector< typename vector<T>::iterator > _ends;
+   vector< vector<T> > *_vv;
+   int _row, _last_row;
 };
 
 template<typename T>
-Iter<T>::Iter(T *i) : _i(i) {}
+Iter<T>::Iter(vector< vector<T> > &vv, Pos p) : _i(vv.begin()->begin()), _row(0),
+ _last_row(vv.size() - 1), _vv(&vv) {
+   for (int i = 0; i < vv.size(); ++i)
+      _ends.push_back(vv[i].end());
+
+   if (p == END) {
+      _row = _last_row = vv.size() - 1;
+      _i = vv[_last_row].end();
+   }
+}
 
 template<typename T>
-Iter<T>::Iter(const Iter &I) : _i(I._i) {}
+Iter<T>::Iter(const Iter &i) : _i(i._i), _ends(i._ends), _row(i._row), 
+ _vv(i._vv), _last_row(i._last_row) {}
 
 template<typename T>
-Iter<T>& Iter<T>::operator=(T *i) {
-   _i = i;
+Iter<T>& Iter<T>::operator=(const Iter &i) {
+   _i = i._i;
+   _ends = i._ends;
+   _row = i._row;
+   _last_row = i._last_row;
+   _vv = i._vv;
    return *this;
 }
 
 template<typename T>
-Iter<T>& Iter<T>::set_iter(T *i) {
-   _i = i;
+Iter<T>& Iter<T>::set_iter(vector< vector<T> > &vv) {
+   _i = vv.begin()->begin();
+   _row = 0;
+   for (int i = 0; i < vv.size(); ++i)
+      _ends.push_back(vv[i].end());
+   _last_row = vv.size() - 1;
+   _vv = &vv;
    return *this;
 }
 
@@ -51,7 +77,8 @@ bool Iter<T>::operator==(const Iter &r) {
 
 template<typename T>
 Iter<T>& Iter<T>::operator++() {
-   ++_i;
+   if (++_i == _ends[_row] && _row != _last_row)
+      _i = (*_vv)[++_row].begin();
    return *this;
 }
 
