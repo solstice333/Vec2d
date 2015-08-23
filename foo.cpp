428e7e981211b28752148662902f15566565f640
Kevin Navero <knavero@gmail.com>
2015-08-23 08:59:43 -0700

    finished that damn iterator stuff

--- foo.cpp
+++ foo.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+// iterator_tests() : tests the public Iter class logic.
 void iterator_tests() {
    vector< vector<int> > vv(2);
    int val = 0;
@@ -78,11 +79,49 @@ void iterator_tests() {
    assert(copy_itr == itr);
 }
 
+// internal_iterator_tests() : tests the iterator interface within Vec2D
+void internal_iterator_tests() {
+   Vec2d<int> matrix(3, 2);
+   Vec2d<int>::iterator test_begin = matrix.begin();
+   Vec2d<int>::iterator test_end = matrix.end();
+
+   for (int i = 0; i < matrix.get_num_rows(); ++i) {
+      for (int j = 0; j < matrix.get_num_cols(); ++j)
+         matrix[i][j] = i + j;
+   }
+
+   cout << "2d vector contents" << endl;
+   for (int i = 0; i < matrix.get_num_rows(); ++i) {
+      for (int j = 0; j < matrix.get_num_cols(); ++j) {
+         if (j > 0)
+            cout << ", ";
+         cout << matrix[i][j];
+      }
+      cout << endl;
+   }
+   cout << "end of contents" << endl;
+
+   int cols = matrix.get_num_cols();
+   int rows = matrix.get_num_rows();
+   int i = 0, j = 0;
+   for (auto itr = matrix.begin(), end = matrix.end(); itr != end; ++itr) {
+      cout << *itr << endl;
+
+      if (i >= cols) {
+         i = 0;
+         ++j;
+      }
+      assert(*itr == matrix[j][i++]);
+   }
+}
+
+// constructor_tests_vec2d() : tests constructor and a few helper methods
 void constructor_tests_vec2d() {
    Vec2d<int> matrix;
    Vec2d<int> matrix1(3, 2);
 
    assert(matrix1.get_num_rows() == 3);
+   assert(matrix1.get_num_cols() == 2);
    matrix1[0][0] = 90;
    assert(matrix1[0][0] == 90);
    matrix1[0][1] = 80;
@@ -94,4 +133,5 @@ void constructor_tests_vec2d() {
 int main() {
    constructor_tests_vec2d();
    iterator_tests();
+   internal_iterator_tests();
 }
--- vec2d.h
+++ vec2d.h
@@ -6,20 +6,24 @@
 
 using namespace std;
 
-
 template<typename T> 
 class Vec2d {
    enum Pos {BEG, END};
 public:
+   class iterator : public Iter<T> {
+   public:
+      iterator(vector< vector<T> > &vv, Pos p = BEG);
+      iterator(const iterator &i);
+   };
+
    Vec2d();
    Vec2d(int row, int col);
    vector<T>& operator[](const int idx);
    int get_num_rows();
+   int get_num_cols();
+   iterator begin();
+   iterator end();
 
-   class iterator : public Iter<T> {
-      iterator(vector< vector<T> > &vv, Pos p = BEG);
-      iterator(const iterator &i);
-   };
 private:
    vector< vector<T> > _v;
 };
@@ -39,13 +43,30 @@ int Vec2d<T>::get_num_rows() {
 }
 
 template<typename T> 
+int Vec2d<T>::get_num_cols() {
+   if (_v.size() > 0)
+      return _v[0].size();
+   return 0;
+}
+
+template<typename T> 
 vector<T>& Vec2d<T>::operator[](const int idx) {
    return _v[idx];
 }
 
 template<typename T> 
+typename Vec2d<T>::iterator Vec2d<T>::begin() {
+   return iterator(this->_v);
+}
+
+template<typename T> 
+typename Vec2d<T>::iterator Vec2d<T>::end() {
+   return iterator(this->_v, END);
+}
+
+template<typename T> 
 Vec2d<T>::iterator::iterator(vector< vector<T> > &vv, Pos p) : 
- Iter<T>(vv, p) {}
+ Iter<T>(vv, static_cast<typename Iter<T>::Pos>(p)) {}
 
 template<typename T> 
 Vec2d<T>::iterator::iterator(const iterator &i) : 
